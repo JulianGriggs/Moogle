47f07597cdc4f2036ad31aa89583057728560150
colemccracken <cmccrack@princeton.edu>
2014-11-24 15:01:04 -0500

    4.5

--- a6/part4/lcs.ml
+++ a6/part4/lcs.ml
@@ -53,8 +53,54 @@ let fast_lcs (ds : dna * dna) : dna =  failwith "unimplemented";;
 (* Implement some experiment that shows performance difference
  * between slow_lcs and fast_lcs. (Print your results.)     
  * Explain in a brief comment what your experiment shows.        *)
+Random.self_init
+
+let test_slow (s1,s2:dna*dna) : float = 
+	time_fun (fun _ -> (slow_lcs (s1,s2))) ()
+
+let test_fast (s1,s2:dna*dna) : float = 
+	time_fun (fun _ -> (fast_lcs (s1,s2))) ()
+
+let print_header () =
+  print_string "-----   Test 1   Test 2   \n";
+  print_string "  N      Slow     Fast    \n";
+  print_string "-----   ------   ------   \n"
+;;
+
+let print_row n test_slow test_fast =
+  let space () = print_string "  " in
+  let float f = Printf.printf "%6.4f" f in
+  if n < 10000 then print_string " ";
+  print_int n;       
+	space (); space();
+  float test_slow;
+  space (); space ();
+  float test_fast; 
+	print_newline()
+
+let experiment (n:int) : unit =
+	let random_base = 
+		let r = Random.int 4 in
+		match r with 
+		| 0 -> Base.A
+		| 1 -> Base.C
+		| 2 -> Base.T
+		| 3 -> Base.G
+		| _ -> failwith "impossible"
+	in
+	let rec generate_dna n = 
+		match n with 
+		| 0 -> []
+		| _ -> random_base :: generate_dna (n-1)
+  in
+	let dna1 = generate_dna n in
+	let dna2 = generate_dna n in
+  print_row n (test_slow (dna1,dna2)) (test_fast (dna1,dna2))
+
 let main () =
-  print_string "nothing yet ...\n"
+  let ns = [1000; 2000; 4000; 8000; 16000; 32000] in
+  print_header();
+  List.iter experiment ns
 ;;
 
 (*
